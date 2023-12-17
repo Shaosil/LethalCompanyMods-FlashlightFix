757bb4915ca9609b5c039cacf9c289a9f7994fd4
Shaosil <bbeck1988@gmail.com>
2023-12-17 12:19:28 -0500

    Updating GUID to match Thunderstore identifier

--- Metadata.cs
+++ Metadata.cs
@@ -2,7 +2,7 @@
 {
     internal static class Metadata
     {
-        public const string GUID = "Shaosil.FlashlightFix";
+        public const string GUID = "ShaosilGaming.FlashlightFix";
         public const string PLUGIN_NAME = "FlashlightFix";
         public const string VERSION = "1.0.0";
     }
